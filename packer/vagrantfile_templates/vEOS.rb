b289f18ddba9767593899958376ad09c5d5f6428
Jere Julian <jerearista@users.noreply.github.com>
2015-04-10 13:00:47 -0400

    Update vagrantfile template
    - Improve comments
    - Create Interfaces Ethernet1 and 2 by default
    - Create a vagrant/vagrant user that can access eapi (or the CLI)

--- packer/vagrantfile_templates/vEOS.rb
+++ packer/vagrantfile_templates/vEOS.rb
@@ -14,12 +14,25 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     config.vm.synced_folder ".", "/vagrant", disabled: true
   end
 
-  # Configure the default ssh username for EOS
+  # In EOS, root, if enabled, is the only user that can ssh directly to bash.
   config.ssh.username = "root"
+
+  # Network Interfaces:  
+  # NIC1 (Management 1) - is created in the the basebox.
+  # Uncomment to create additional NICs which will be Ethernet 1-n                                
+  #   Using link-local addresses to satisfy the Vagrantfile config parser, only.
+  #config.vm.network "private_network", ip: "192.168.1.3", auto_config: false, virtualbox__intnet: true
+  #config.vm.network "private_network", virtualbox__intnet: "mynetwork-1", ip: "169.254.1.11", auto_config: false
+  # Create Ethernet1
+  config.vm.network "private_network", virtualbox__intnet: true, ip: "169.254.1.11", auto_config: false
+  # Create Ethernet2
+  config.vm.network "private_network", virtualbox__intnet: true, ip: "169.254.1.11", auto_config: false
+
 
   # Enable eAPI in the EOS config
   config.vm.provision "shell", inline: <<-SHELL
     FastCli -p 15 -c "configure
+    username vagrant privilege 15 role network-admin secret vagrant
     management api http-commands
       no shutdown
     end
@@ -28,6 +41,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   # Enable eAPI over HTTP in the EOS config
   #  FastCli -p 15 -c "configure
+  #  username vagrant privilege 15 role network-admin secret vagrant
   #  management api http-commands
   #    no protocol https
   #    protocol http
