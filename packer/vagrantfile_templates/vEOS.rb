b737b9b92f7562d9a8ede45856434fcbec659066
Jere Julian <jere@arista.com>
2016-12-21 14:37:06 -0500

    Change double-quotes to single

--- packer/vagrantfile_templates/vEOS.rb
+++ packer/vagrantfile_templates/vEOS.rb
@@ -2,17 +2,17 @@
 # vi: set ft=ruby :
 
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
-VAGRANTFILE_API_VERSION = "2"
+VAGRANTFILE_API_VERSION = '2'
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Every Vagrant virtual environment requires a box to build off of.
-  #config.vm.box = "vEOS_4.14.4F"
+  # config.vm.box = 'vEOS_4.14.4F'
 
   config.vm.provider 'virtualbox' do |_, override|
     # Disable synced folders
-    config.vm.synced_folder ".", "/vagrant", disabled: true
+    config.vm.synced_folder '.', '/vagrant', disabled: true
   end
 
   # In EOS, root, if enabled, is the only user that can ssh directly to bash.
-  config.ssh.username = "root"
+  config.ssh.username = 'root'
 end
